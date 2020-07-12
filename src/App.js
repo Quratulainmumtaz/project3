996865c4ad24c04a717d82ab572a8bf2f72c21b3
Qurat Ul Ain Mumtaz <mcs.qurat@gmail.com>
2020-07-12 12:51:47 +0500

    update the work

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br>Update by Qurat Ul Ain Mumtaz</br>
         </p>
         <a
           className="App-link"
